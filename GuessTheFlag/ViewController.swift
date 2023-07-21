52dd4eb14a385df80c8edec32cbb480edbbbb0e5
Linconl Rufino <linconldasilvarufino@gmail.com>
2023-07-21 19:29:18 -0300

    From outlets to actions: creating an IBAction

--- GuessTheFlag/ViewController.swift
+++ GuessTheFlag/ViewController.swift
@@ -24,6 +24,37 @@ class ViewController: UIViewController {
         askQuestion()
     }
     
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        var title: String
+        
+        if sender.tag == correctAnswer {
+            title = "Correct"
+            score += 1
+        }else {
+            title = "Wrong"
+            score -= 1
+        }
+        
+        let ac = UIAlertController(title: title, message: "Your score is \(score)", preferredStyle: .alert)
+        
+        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        
+        present(ac, animated: true)
+        
+    }
+    
+    func askQuestion (action: UIAlertAction! = nil) {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+        
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
+        
+        title = countries[correctAnswer].uppercased()
+    }
+    
     func configureCountries() {
         countries += ["estonia", "france", "germany",
                       "ireland", "italy", "monaco", "nigeria",
@@ -40,17 +71,6 @@ class ViewController: UIViewController {
         button3.layer.borderColor = UIColor.lightGray.cgColor
         
         navigationController?.navigationBar.titleTextAttributes = [.foregroundColor :UIColor.black]
-    }
-    
-    func askQuestion () {
-        countries.shuffle()
-        correctAnswer = Int.random(in: 0...2)
-        
-        button1.setImage(UIImage(named: countries[0]), for: .normal)
-        button2.setImage(UIImage(named: countries[1]), for: .normal)
-        button3.setImage(UIImage(named: countries[2]), for: .normal)
-        
-        title = countries[correctAnswer].uppercased()
     }
     
 }
